2566106283e08f99f8c4077bd73015a310bf1e79
Eric Fritz <eric@eric-fritz.com>
2017-08-14 21:36:45 -0500

    Add election implementation.

---
+++ election.go
@@ -0,0 +1,76 @@
+package reception
+
+import (
+	"errors"
+
+	"github.com/satori/go.uuid"
+)
+
+type (
+	Elector interface {
+		Elect() error
+		Cancel()
+	}
+
+	elector struct {
+		client   Client
+		name     string
+		metadata Metadata
+		stop     chan struct{}
+	}
+)
+
+var ErrElectionCanceled = errors.New("election canceled")
+
+func NewElector(client Client, name string, metadata Metadata) Elector {
+	return &elector{
+		client:   client,
+		name:     name,
+		metadata: metadata,
+		stop:     make(chan struct{}),
+	}
+}
+
+func (e *elector) Elect() error {
+	service := &Service{
+		ID:       uuid.NewV4().String(),
+		Name:     e.name,
+		Metadata: e.metadata,
+	}
+
+	if err := e.client.Register(service); err != nil {
+		return err
+	}
+
+	watcher := e.client.NewWatcher(e.name)
+	ch, err := watcher.Start()
+	if err != nil {
+		return err
+	}
+
+	defer watcher.Stop()
+
+loop:
+	for {
+		select {
+		case <-e.stop:
+			return ErrElectionCanceled
+
+		case services := <-ch:
+			if len(services) > 0 && services[0].ID == service.ID {
+				break loop
+			}
+		}
+	}
+
+	go func() {
+		for range ch {
+		}
+	}()
+
+	return nil
+}
+
+func (e *elector) Cancel() {
+	close(e.stop)
+}
---
+++ election_test.go
@@ -0,0 +1,134 @@
+package reception
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+type ElectionSuite struct{}
+
+func (s *ElectionSuite) TestElect(t *testing.T) {
+	var (
+		client            = newMockClient()
+		watcher           = newMockWatcher()
+		serviceChan       = make(chan []*Service)
+		errChan           = make(chan error)
+		registeredService *Service
+		stopCalled        = false
+		elector           = NewElector(client, "service", map[string]string{
+			"foo": "bar",
+			"baz": "bonk",
+		})
+	)
+
+	defer close(errChan)
+
+	client.register = func(service *Service) error {
+		registeredService = service
+		return nil
+	}
+
+	client.newWatcher = func(service string) Watcher {
+		return watcher
+	}
+
+	watcher.start = func() (<-chan []*Service, error) {
+		return serviceChan, nil
+	}
+
+	watcher.stop = func() error {
+		stopCalled = true
+		return nil
+	}
+
+	go func() {
+		errChan <- elector.Elect()
+	}()
+
+	serviceChan <- []*Service{}
+	Consistently(errChan).ShouldNot(Receive())
+
+	serviceChan <- []*Service{&Service{ID: "wrong id"}}
+	Consistently(errChan).ShouldNot(Receive())
+
+	serviceChan <- []*Service{&Service{ID: registeredService.ID}}
+	Eventually(errChan).Should(Receive(BeNil()))
+
+	Expect(stopCalled).To(BeTrue())
+	Expect(registeredService.Name).To(Equal("service"))
+	Expect(registeredService.Metadata).To(Equal(Metadata(map[string]string{
+		"foo": "bar",
+		"baz": "bonk",
+	})))
+}
+
+func (s *ElectionSuite) TestCancel(t *testing.T) {
+	var (
+		client     = newMockClient()
+		watcher    = newMockWatcher()
+		errChan    = make(chan error)
+		stopCalled = false
+		elector    = NewElector(client, "service", nil)
+	)
+
+	defer close(errChan)
+
+	client.newWatcher = func(service string) Watcher {
+		return watcher
+	}
+
+	watcher.start = func() (<-chan []*Service, error) {
+		return make(chan []*Service), nil
+	}
+
+	watcher.stop = func() error {
+		stopCalled = true
+		return nil
+	}
+
+	go func() {
+		errChan <- elector.Elect()
+	}()
+
+	Consistently(errChan).ShouldNot(Receive())
+	elector.Cancel()
+	Eventually(errChan).Should(Receive(Equal(ErrElectionCanceled)))
+	Expect(stopCalled).To(BeTrue())
+}
+
+//
+// Mocks
+
+type mockClient struct {
+	register     func(service *Service) error
+	listServices func(name string) ([]*Service, error)
+	newWatcher   func(name string) Watcher
+}
+
+func newMockClient() *mockClient {
+	return &mockClient{
+		register:     func(service *Service) error { return nil },
+		listServices: func(name string) ([]*Service, error) { return nil, nil },
+		newWatcher:   func(name string) Watcher { return nil },
+	}
+}
+
+func (c *mockClient) Register(service *Service) error              { return c.register(service) }
+func (c *mockClient) ListServices(name string) ([]*Service, error) { return c.listServices(name) }
+func (c *mockClient) NewWatcher(name string) Watcher               { return c.newWatcher(name) }
+
+type mockWatcher struct {
+	start func() (<-chan []*Service, error)
+	stop  func() error
+}
+
+func newMockWatcher() *mockWatcher {
+	return &mockWatcher{
+		start: func() (<-chan []*Service, error) { return nil, nil },
+		stop:  func() error { return nil },
+	}
+}
+
+func (w *mockWatcher) Start() (<-chan []*Service, error) { return w.start() }
+func (w *mockWatcher) Stop() error                       { return w.stop() }
